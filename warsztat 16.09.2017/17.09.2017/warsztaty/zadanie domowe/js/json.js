e041cfbc82b29f3ddc295669646312f57315dc5b
Juszczak Marcin <juszczak-macin@wp.pl>
2017-09-23 08:16:37 +0200

    poprawiony AJAX

--- warsztat 16.09.2017/17.09.2017/warsztaty/zadanie domowe/js/json.js
+++ warsztat 16.09.2017/17.09.2017/warsztaty/zadanie domowe/js/json.js
@@ -4,14 +4,15 @@ $(document).ready( function() {
     
     $( "button" ).click( function() {
         
-    $.getJSON("https://echo.jsontest.com/imie/Piotr/nazwisko/Lewinski/zawod/Programista/firma/Akademia108", function (data) {
+    $.getJSON('http://echo.jsontest.com/imie/Piotr/nazwisko/Lewinski/zawod/Programista/firma/Akademia108', function (data) {
         
     console.log(data);
     
-   $("body").append("<div id=dane-programisty>Imię: "+ data.imie+"</div>");    
-    $("body").append("<div id=dane-programisty>Nazwisko: "+ data.nazwisko+"</div>"); 
-    $("body").append("<div id=dane-programisty>Zawód: "+ data.zawod+"</div>");
-     $("body").append("<div id=dane-programisty>Firma: "+ data.firma+"</div>");
+   $("body").append("<div id=dane-programisty>"+ 
+    "<p>Imię: " +   data.imie+"</p>"+ 
+    "<p>Nazwisko: "+ data.nazwisko+"</p>"+ 
+    "<p>Zawód: "+ data.zawod+"</p>" +
+    "<p>Firma: "+ data.firma+ "</p>"+"</div>");
 } );
 } );
 });
