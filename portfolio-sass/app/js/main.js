2d4620fe0dccef41f4f867a0892e4640ed997120
Juszczak Marcin <marcin-juszczak@wp.pl>
2017-10-05 09:39:07 +0200

    sass

---
+++ portfolio-sass/app/js/main.js
@@ -0,0 +1,27 @@
+'use strict';
+$(document).ready(function() {
+    scrollMenu();
+    smoothScroll();
+});
+
+$(window).scroll(function() {
+    scrollMenu();
+});
+
+function smoothScroll() {
+    $('a[href*="#"]:not([href="#"])').click(function(){
+		$('body').animate( { scrollTop: $(this.hash).offset().top - 50 }, 800 );
+	});
+}
+
+
+function scrollMenu() {
+    var menuHeight = $('#main-nav').height();
+    var scrolled = $(window).scrollTop();
+    
+    if(scrolled >= menuHeight) {
+        $('#main-nav').addClass('scrolled');
+    } else {
+        $('#main-nav').removeClass('scrolled');
+    }
+}
